17cc5b86f823260487a8b3110e0019dd99c20047
Kamila0888 <kamilekahtyam@mail.ru>
2026-09-18 00:34:06 +0500

    Delete dz-akhtyamova directory

--- dz-akhtyamova/content/script/main.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector('.navigation-mobile')
-const burger = document.querySelector('.burger')
-
-burger.addEventListener('click',()=>{
-    menu.classList.toggle('navigation--open')
-    burger.classList.toggle('burger--open')
-});
